cf87c1d8811990c258c59e6814e8c05ef340ee63
irisyen115 <you@example.com>
2022-05-01 11:46:07 +0800

    add bj

---
+++ bj.js
@@ -0,0 +1,6 @@
+function Onclick(){
+    const parent = document.getElementById("player_desk");
+    const child = document.createElement("div");
+    child.setAttribute("class","child");
+    parent.appendChild(child);
+ }
